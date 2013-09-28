ae364e3a11f79ba9bf3b2bf3957b29940709ee99
Cameron Hunter <hello@cameronhunter.co.uk>
2013-09-28 14:24:36 -0700

    Simplify

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,11 @@ module.exports = function(grunt) {
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
   grunt.initConfig({
-
     "gh-pages": {
       options: {
         base: "app"
       },
       src: ["**"]
     }
-
   });
 };
