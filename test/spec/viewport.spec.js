879ecc9f3c592d737cd355fd019cbabd43f1b6c9
Cameron Hunter <hello@cameronhunter.co.uk>
2013-09-28 00:07:09 -0700

    Adding tests

--- test/spec/viewport.spec.js
+++ test/spec/viewport.spec.js
@@ -2,13 +2,54 @@
 
 describeComponent('lib/viewport', function () {
 
-  // Initialize the component and attach it to the DOM
+  var S = {'max-width': 865};
+  var M = {'min-width': 866, 'max-width': 1024};
+  var L = {'min-width': 1025, 'max-width': 1600};
+  var XL = {'min-width': 1601};
+
   beforeEach(function () {
-    setupComponent();
+    setupComponent({
+      breakpoints: [S, M, L, XL]
+    });
   });
 
-  it('should be defined', function () {
-    expect(this.component).toBeDefined();
+  it('should be trigger a viewport update when the window resizes', function () {
+    var eventSpy = spyOnEvent(document, 'viewport-update');
+
+    this.component.trigger('viewport-resize', {width: 1000});
+
+    expect(eventSpy).toHaveBeenTriggeredOn(document);
+    expect(eventSpy.mostRecentCall.data).toEqual(M);
+
+    this.component.trigger('viewport-resize', {width: 2000});
+
+    expect(eventSpy).toHaveBeenTriggeredOn(document);
+    expect(eventSpy.mostRecentCall.data).toEqual(XL);
+
+    this.component.trigger('viewport-resize', {width: 500});
+
+    expect(eventSpy).toHaveBeenTriggeredOn(document);
+    expect(eventSpy.mostRecentCall.data).toEqual(S);
+
+    this.component.trigger('viewport-resize', {width: 1500});
+
+    expect(eventSpy).toHaveBeenTriggeredOn(document);
+    expect(eventSpy.mostRecentCall.data).toEqual(L);
+  });
+
+  it('should not repeatedly trigger the viewport update', function() {
+    var eventSpy = spyOnEvent(document, 'viewport-update');
+
+    expect(eventSpy.callCount).toEqual(0);
+
+    this.component.trigger('viewport-resize', {width: 1000});
+
+    expect(eventSpy.callCount).toEqual(1);
+    expect(eventSpy.mostRecentCall.data).toEqual(M);
+
+    this.component.trigger('viewport-resize', {width: 1000});
+
+    expect(eventSpy.callCount).toEqual(1);
   });
 
 });
