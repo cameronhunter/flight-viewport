4b14bdc02360253c7733c68934f670032319757f
Cameron Hunter <hello@cameronhunter.co.uk>
2013-09-28 13:08:18 -0700

    Adding grunt and grunt-gh-pages builder

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt) {
+
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  grunt.initConfig({
+
+    "gh-pages": {
+      options: {
+        base: "app"
+      },
+      src: ["**"]
+    }
+
+  });
+};
